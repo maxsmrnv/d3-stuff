e39b648a9591a0317518a600d6e0cfa41715eb73
Maksim.Smirnov <Maksim.Smirnov@nexign.com>
2019-10-24 15:29:20 +0300

    more stuff

--- react-d3/src/App.js
+++ react-d3/src/App.js
@@ -1,115 +1,165 @@
-import React, { useRef, useEffect, useState } from 'react';
-import './App.css';
-import { D3Container } from './d3';
+import React, { useRef, useEffect, useState } from 'react'
+import './App.css'
+import { D3Container } from './d3'
 
 const symbols = [
   {
     type: 'symbolCircle',
-    data: [[300, 300], [-300, -300],[-300, 300], [300, -300]],
+    data: [[300, 300], [-300, -300], [-300, 300], [300, -300]],
+    dataWithId: [
+      {
+        id: 1,
+        position: [300, 300],
+      },
+      {
+        id: 2,
+        position: [-300, -300],
+      },
+      {
+        id: 3,
+        position: [-300, 300],
+      },
+      {
+        id: 4,
+        position: [300, -300],
+      },
+    ],
   },
   {
     type: 'symbolTriangle',
     data: [[0, 0], [0, 850], [0, -850], [850, 0], [-850, 0]],
+    dataWithId: [
+      {
+        id: 1,
+        position: [0, 0],
+      },
+      {
+        id: 2,
+        position: [0, 850],
+      },
+      {
+        id: 3,
+        position: [0, -850],
+      },
+      {
+        id: 4,
+        position: [850, 0],
+      },
+      {
+        id: 4,
+        position: [-850, 0],
+      },
+    ],
   },
-];
+]
 
 const lines = {
-    type: 'line',
-    data: symbols[0].data.map(v => [[v[0]-150, v[1]],[v[0]+150, v[1]]] ),
-  }
+  type: 'line',
+  data: symbols[0].data.map(v => [[v[0] - 150, v[1]], [v[0] + 150, v[1]]]),
+}
 
-  const circleContour = {
-    type: 'circleContour',
-    x: 0,
-    y: 0,
-    r: 300
-  }
+const circleContour = {
+  type: 'circleContour',
+  x: 0,
+  y: 0,
+  r: 500,
+}
 
-  const rectContour = {
-    type: 'rectContour',
-    x: -400,
-    y: 400,
-    w: 400,
-    h: 400
-  }
+const rectContour = {
+  type: 'rectContour',
+  x: -400,
+  y: 200,
+  w: 800,
+  h: 400,
+}
 
-  const polyContour = {
-    type: 'polyContour',
-    points: [[[0,1000],[-800,-900],[1000, -1000]]]
+const polyContour = {
+  type: 'polyContour',
+  points: [[[0, 1000], [-800, -900], [1000, -1000]]],
+}
 
-  }
+const contours = [circleContour, rectContour, polyContour]
 
-  const contours = [circleContour,rectContour,polyContour]
+const polyLayers = [
+  [[0, -1000], [-1000, -400], [-800, 900], [800, 800], [1000, 200]],
+]
 
-  const polyLayers = [[[0,-1000],[-1000, -400],[-800,900],[800, 800],[1000, 200]]]
-
-
-const d3magic = new D3Container();
-
+const d3magic = new D3Container()
 
 export const App = () => {
-  const d3Container = useRef(null);
-  const [symbolsState, setSymbols] = useState(symbols);
-  const [linesState, setLines] = useState(lines);
-  const [contourState, setContour] = useState(rectContour.type);
-  const [polyLayersState, setPolyLayers] = useState(polyLayers);
+  const d3Container = useRef(null)
+  const [symbolsState, setSymbols] = useState(symbols)
+  const [linesState, setLines] = useState(lines)
+  const [contourState, setContour] = useState(rectContour.type)
+  const [polyLayersState, setPolyLayers] = useState(polyLayers)
 
   useEffect(() => {
-    d3magic.init(d3Container.current);
-    d3magic.drawPolyLayers(polyLayersState);
-    d3magic.drawSymbols(symbolsState);
-    d3magic.drawLine(linesState);
-  }, []);
+    d3magic.init(d3Container.current)
+    d3magic.drawPolyLayers(polyLayersState)
+    d3magic.drawLine(linesState)
+    d3magic.drawSymbols(symbolsState)
+  }, [linesState, polyLayersState, symbolsState])
 
   useEffect(() => {
-    d3magic.drawContour(contours.find(v => v.type === contourState));
-  }, [contourState]);
-
-
+    d3magic.drawContour(contours.find(v => v.type === contourState))
+  }, [contourState])
 
   const handleReset = () => {
-    d3magic.reset()
-  };
-const handleChangeContour = e => {
-  setContour(e.target.value)
-}
+    d3magic.resetZoomPan()
+  }
+  const handleChangeContour = e => {
+    setContour(e.target.value)
+  }
 
   return (
     <div className="App">
-      <div
-        style={{ width: '100%', height: '800px' }}
-        ref={d3Container}
-      />
+      <div style={{ width: '100%', height: '800px' }} ref={d3Container} />
       <div>
-      <hr/>
-      <button onClick={handleReset}>ZOOM PAN RESET</button>
-      <hr/>
-      {symbolsState.map(v => 
-      <h5 key={v.type}>
-        {v.type}
-        <input readOnly style={{width: '500px'}}  value={JSON.stringify(v.data)}/>
-      </h5>
-      )}
-      <hr/>
-      <h5>
-        {lines.type}
-        <input readOnly style={{width: '500px'}}  value={JSON.stringify(lines.data)}/>
-      </h5>
-      <hr/>
-      <h5>
-      Contour
-      <br/>
-        <select value={contourState} onChange={handleChangeContour}>
-          {contours.map(v => <option key={v.type} value={v.type}>{JSON.stringify(v)}</option>)}
-        </select>
-      </h5>
-      <hr/>
-      <h5>
-        polyLayers
-        <input readOnly style={{width: '500px'}}  value={JSON.stringify(polyLayersState)}/>
-      </h5>
-      <hr/>
+        <hr />
+        <button onClick={handleReset}>ZOOM PAN RESET</button>
+        <hr />
+        {symbolsState.map(v => (
+          <h5 key={v.type}>
+            {v.type}
+            <input
+              readOnly
+              style={{ width: '500px' }}
+              value={JSON.stringify(v.data)}
+            />
+          </h5>
+        ))}
+        <hr />
+        <h5>
+          {lines.type}
+          <input
+            readOnly
+            style={{ width: '500px' }}
+            value={JSON.stringify(lines.data)}
+          />
+        </h5>
+        <hr />
+        <h5>
+          Contour
+          <br />
+          <select value={contourState} onChange={handleChangeContour}>
+            {contours.map(v => (
+              <option key={v.type} value={v.type}>
+                {JSON.stringify(v)}
+              </option>
+            ))}
+          </select>
+        </h5>
+        <hr />
+        <h5>
+          polyLayers
+          <input
+            readOnly
+            style={{ width: '500px' }}
+            value={JSON.stringify(polyLayersState)}
+          />
+        </h5>
+        <hr />
       </div>
     </div>
-  );
-};
+  )
+}
--- react-d3/src/d3/index.js
+++ react-d3/src/d3/index.js
@@ -1,24 +1,25 @@
-import * as d3 from 'd3';
+import * as d3 from 'd3'
 
 export class D3Container {
   constructor() {
-    this.scatter = null;
-    this.x = null;
-    this.y = null;
-    this.transform = null;
-    this.update = null;
-    this.container = null;
-    this.zoom_function = null;
+    this.scatter = null
+    this.x = null
+    this.y = null
+    this.z = null
+    this.transform = null
+    this.update = null
+    this.container = null
+    this.zoom_function = null
   }
   init(selector) {
     // d3.select('svg > *').remove();
-    const margin = { top: 100, right: 100, bottom: 100, left: 100 };
-    const outerWidth = 800;
-    const outerHeight = 800;
-    const width = outerWidth - margin.left - margin.right;
-    const height = outerHeight - margin.top - margin.bottom;
+    const margin = { top: 100, right: 100, bottom: 100, left: 100 }
+    const outerWidth = 800
+    const outerHeight = 800
+    const width = outerWidth - margin.left - margin.right
+    const height = outerHeight - margin.top - margin.bottom
 
-    this.container = d3.select(selector);
+    this.container = d3.select(selector)
 
     // Init SVG
     const svgChart = this.container
@@ -27,7 +28,7 @@ export class D3Container {
       .attr('height', outerHeight)
       .attr('class', 'svg-plot')
       .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
     // Add a clipPath: everything out of this area won't be drawn.
 
@@ -39,175 +40,197 @@ export class D3Container {
       .attr('width', width)
       .attr('height', height)
       .attr('x', 0)
-      .attr('y', 0);
+      .attr('y', 0)
 
     // Create the this.scatter variable: where both the circles and the brush take place
-    this.scatter = svgChart.append('g').attr('clip-path', 'url(#clip)');
+    this.scatter = svgChart.append('g').attr('clip-path', 'url(#clip)')
 
     // Init Scales
     this.x = d3
       .scaleLinear()
       .domain([-1100, 1100])
       .range([0, width])
-      .nice();
+      .nice()
 
     this.y = d3
       .scaleLinear()
       .domain([-1100, 1100])
       .range([height, 0])
-      .nice();
+      .nice()
+
+    this.z = d3
+      .scaleLinear()
+      .domain([0, 2400])
+      .range([0, width])
+      .nice()
 
     // Init Axis
-    const xAxis = d3.axisTop(this.x).tickSize(2);
-    const yAxis = d3.axisLeft(this.y).tickSize(2);
+    const xAxis = d3.axisTop(this.x).tickSize(2)
+    const yAxis = d3.axisLeft(this.y).tickSize(2)
 
     // Add Axis
-    const gxAxis = svgChart.append('g').call(xAxis);
-    const gyAxis = svgChart.append('g').call(yAxis);
-
+    const gxAxis = svgChart.append('g').call(xAxis)
+    const gyAxis = svgChart.append('g').call(yAxis)
 
     // Update plot on canvas
     this.update = transform => {
-      this.transform = transform;
-      const scaleX = transform.rescaleX(this.x);
-      const scaleY = transform.rescaleY(this.y);
+      this.transform = transform
+      const scaleX = transform.rescaleX(this.x)
+      const scaleY = transform.rescaleY(this.y)
 
-      gxAxis.call(xAxis.scale(scaleX));
-      gyAxis.call(yAxis.scale(scaleY));
+      gxAxis.call(xAxis.scale(scaleX))
+      gyAxis.call(yAxis.scale(scaleY))
 
-  
-      this.scatter
-        .selectAll('circle')
-        .attr('transform', transform);
+      this.scatter.selectAll('.contour').attr('transform', transform)
 
-        this.scatter
-        .selectAll('polygon')
-        .attr('transform', transform);
+      this.scatter.selectAll('.polyLayer').attr('transform', transform)
 
-        this.scatter
-        .selectAll('rect')
-        .attr('transform', transform);
+      this.scatter.selectAll('.line').attr('transform', transform)
 
+      this.scatter.selectAll('.symbol').attr('transform', d => {
+        //   const curTransform = d3.select(this).attr('transform')
 
-      this.scatter.selectAll('.points').attr('transform', d => {
+        //   function getTranslation(transform) {
+        //     // Create a dummy g for calculation purposes only. This will never
+        //     // be appended to the DOM and will be discarded once this function
+        //     // returns.
+        //     var g = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+
+        //     // Set the transform attribute to the provided string value.
+        //     g.setAttributeNS(null, 'transform', transform)
+
+        //     // consolidate the SVGTransformList containing all transformations
+        //     // to a single SVGTransform of type SVG_TRANSFORM_MATRIX and get
+        //     // its SVGMatrix.
+        //     var matrix = g.transform.baseVal.consolidate().matrix
+
+        //     // As per definition values e and f are the ones for the translation.
+        //     return [matrix.e, matrix.f]
+        //   }
+
         return `translate(
             ${transform.x + this.x(d[0]) * transform.k},
-            ${transform.y + this.y(d[1]) * transform.k})`;
-      });
-
-      this.scatter.selectAll('.line').attr('transform', transform);
-
-    };
+            ${transform.y + this.y(d[1]) * transform.k})`
+      })
+    }
 
     // Zoom/Drag handler
     this.zoom_function = d3
       .zoom()
       .scaleExtent([-100, 100])
       .on('zoom', () => {
-        this.update(d3.event.transform);
-      });
+        this.update(d3.event.transform)
+      })
 
-    this.container.call(this.zoom_function);
-
+    this.container.call(this.zoom_function)
   }
 
-
-  reset = () => {
-    this.container.transition().duration(500).call(this.zoom_function.transform, d3.zoomIdentity)
+  resetZoomPan = () => {
+    this.container
+      .transition()
+      .duration(500)
+      .call(this.zoom_function.transform, d3.zoomIdentity)
   }
 
-  drawLine = data => {
-    const line = d3.line().x(d => this.x(d[0])).y(d => this.y(d[1]))
+  drawLine = ({ data }) => {
+    const line = d3
+      .line()
+      .x(d => this.x(d[0]))
+      .y(d => this.y(d[1]))
     this.scatter
-    .append('g')
-    .attr('class', 'lines')
-    .selectAll('path')
-    .data(data.data)
-    .enter()
-    .append('path')
-    .attr('d', d => line(d))
-    .attr('class', 'line')
-    .style("fill", "none")
-    .style("stroke", "brown")
-    .style("stroke-width", 2);
+      .append('g')
+      .attr('class', 'lines')
+      .selectAll('path')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('d', d => line(d))
+      .attr('class', 'line')
+      .style('fill', 'none')
+      .style('stroke', 'brown')
+      .style('stroke-width', 1)
   }
 
-  drawContour = (data)=> {
-    this.scatter.selectAll('.contour').remove();
-    const drawCircle = ({x,y,r}) => {
-      this.scatter
+  drawContour = data => {
+    this.scatter.selectAll('.contourContainer').remove()
+
+    const baseContour = this.scatter
       .append('g')
-      .attr('class', 'contour')
-      .append('circle')
-      .attr('cx', this.x(x))
-      .attr('cy', this.y(y))
-      .attr('r', r)
-      .attr('class', 'circle')
-      .style("fill", "none")
-      .style("stroke", "navy")
-      .style("stroke-width", 1);
+      .attr('class', 'contourContainer')
+
+    const drawCircle = ({ x, y, r }) => {
+      baseContour
+        .append('circle')
+        .attr('class', 'contour')
+        .attr('cx', this.x(x))
+        .attr('cy', this.y(y))
+        .attr('r', this.z(r))
+        .style('fill', 'none')
+        .style('stroke', 'navy')
+        .style('stroke-width', 1)
     }
 
-    const drawRect = ({x,y,w,h}) => {
-      this.scatter
-      .append('g')
-      .attr('class', 'contour')
-      .append('rect')
-      .attr('x', this.x(x))
-      .attr('y', this.y(y))
-      .attr('width', this.y(w))
-      .attr('height', this.y(h))
-      .attr('class', 'rect')
-      .style("fill", "none")
-      .style("stroke", "navy")
-      .style("stroke-width", 1);
+    const drawRect = ({ x, y, w, h }) => {
+      baseContour
+        .append('rect')
+        .attr('class', 'contour')
+        .attr('x', this.x(x))
+        .attr('y', this.y(y))
+        .attr('width', this.z(w))
+        .attr('height', this.z(h))
+        .style('fill', 'none')
+        .style('stroke', 'navy')
+        .style('stroke-width', 1)
     }
 
-    const drawPolyContour = ({points}) => {
-      this.scatter
-      .append('g')
-      .attr('class', 'contour')
-      .selectAll("polygon")
-      .data(points)
-      .enter()
-      .append('polygon')
-      .attr("points",d => d.map(d => [this.x(d[0]),this.y(d[1])].join(",")).join(" "))
-      .style("fill", "none")
-      .style("stroke", "navy")
-      .style("stroke-width", 1);
+    const drawPolyContour = ({ points }) => {
+      baseContour
+        .selectAll('polygon')
+        .data(points)
+        .enter()
+        .append('polygon')
+        .attr('class', 'contour')
+        .attr('points', d =>
+          d.map(d => [this.x(d[0]), this.y(d[1])].join(',')).join(' ')
+        )
+        .style('fill', 'none')
+        .style('stroke', 'navy')
+        .style('stroke-width', 1)
     }
 
-    switch(data.type){
-      case 'rectContour': 
+    switch (data.type) {
+      case 'rectContour':
         drawRect(data)
-        break;
-      case 'circleContour': 
+        break
+      case 'circleContour':
         drawCircle(data)
-        break;
-      case 'polyContour': 
-      drawPolyContour(data)
-        break;
+        break
+      case 'polyContour':
+        drawPolyContour(data)
+        break
       default:
-        console.log('undefined contour type');
+        console.log('undefined contour type')
     }
 
     if (this.transform) {
-      this.update(this.transform);
+      this.update(this.transform)
     }
-
   }
 
   drawPolyLayers = data => {
     this.scatter
-    .append('g')
-    .attr('class', 'layers')
-    .selectAll("polygon")
-    .data(data)
-    .enter()
-    .append('polygon')
-    .attr("points",d => d.map(d => [this.x(d[0]),this.y(d[1])].join(",")).join(" "))
-    .style("fill", "green")
-    .style("opacity", 0.2)
+      .append('g')
+      .attr('class', 'layers')
+      .selectAll('polygon')
+      .data(data)
+      .enter()
+      .append('polygon')
+      .attr('class', 'polyLayer')
+      .attr('points', d =>
+        d.map(d => [this.x(d[0]), this.y(d[1])].join(',')).join(' ')
+      )
+      .style('fill', 'green')
+      .style('opacity', 0.2)
   }
 
   drawSymbols = data => {
@@ -215,31 +238,62 @@ export class D3Container {
     const rules = {
       symbolTriangle: {
         render: d3.symbolTriangle,
-        color: 'blue'
+        color: 'blue',
       },
       symbolCircle: {
         render: d3.symbolCircle,
-        color:'red'
+        color: 'red',
       },
     }
 
+    function dragstarted() {
+      d3.select(this)
+        .raise()
+        .attr('stroke', 'black')
+        .attr('cursor', 'grabbing')
+    }
+
+    function dragended() {
+      d3.select(this)
+        .attr('stroke', null)
+        .attr('cursor', 'grab')
+    }
+
+    const dragged = context => {
+      d3.event.sourceEvent.stopPropagation() // silence other listeners
+      const x = d3.event.x
+      const y = d3.event.y
+      d3.select(context).attr('transform', `translate(${x},${y})`)
+    }
+
+    var dragBehavior = d3
+      .drag()
+      .on('start', dragstarted)
+      .on('drag', function() {
+        return dragged(this)
+      })
+      .on('end', dragended)
+
     data.forEach(d => {
       this.scatter
-      .append('g')
-      .attr('class', 'symbols')
-      .selectAll('path')
-      .data(d.data)
+        .append('g')
+        .attr('class', 'symbols')
+        .selectAll('path')
+        .data(d.data)
         .enter()
         .append('path')
         .attr('d', d3.symbol().type(rules[d.type].render))
-        .attr('class', 'points')
+        .attr('class', 'symbol')
         .attr('fill', rules[d.type].color)
-        .attr('transform', d => `translate(${this.x(d[0])}, ${this.y(d[1])})`);
-    });
+        .attr('transform', d => {
+          return `translate(${this.x(d[0])}, ${this.y(d[1])})`
+        })
+        .attr('cursor', 'grab')
+        .call(dragBehavior)
+    })
 
-    if (this.transform) {
-      this.update(this.transform);
-    }
-  };
-
+    // if (this.transform) {
+    //   this.update(this.transform);
+    // }
+  }
 }
